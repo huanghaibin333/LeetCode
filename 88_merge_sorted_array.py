376c0d91067d7a678e0308a14894685146ca1c65
huanghaibin333 <huanghaibin333@foxmail.com>
2015-01-21 16:22:17 +0800

    add  83

--- 88_merge_sorted_array.py
+++ 88_merge_sorted_array.py
@@ -7,7 +7,7 @@
 # Create Date: 15-1-13
 
 
-def merge(A, m, B, n):
+def merge1(A, m, B, n):
     if m == 0:
         for b in B:
             A.append(b)
@@ -33,12 +33,38 @@ def merge(A, m, B, n):
             j += 1
 
 
+
+def merge(A, m, B, n):
+    if m == 0 and n == 0:
+        pass
+    elif m < n:
+        A, m, B, n
+
+    i = 0
+    for b in B:
+        j = m - 1
+        if b <= A[i]:
+            A.insert(i, b)
+        elif b > A[j]:
+            A.append(b)
+        else:
+            while (j - i) >1:
+                # print i, j
+                k = (i+j)/2
+                if b <= A[k]:
+                    j = k
+                else:
+                    i = k
+            i = (i+j)/2 + 1
+            j += 1
+            A.insert(i, b)
+
+
 if __name__ == '__main__':
     A = []
-    B = [0, 2, 4, 6, 9, 10, 11, 12, 26, 35, 43, 56, 100, 120]
+    C = [0, 2, 4, 6, 9, 10, 11, 12, 26, 35, 43, 56, 100, 120]
+    B = [1]
     print A
     print B
-    merge(A, len(A), B, len(B))
-    print A
-    del A[::]
+    merge1(A, len(A), B, len(B))
     print A
